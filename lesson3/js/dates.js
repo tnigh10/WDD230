ce1498d521c8f1173dbce1581e8ab8d59bfd5308
tnigh10 <tnigh10@byui.edu>
2022-01-22 16:23:12 -0600

    pages

--- lesson3/js/dates.js
+++ lesson3/js/dates.js
@@ -1,3 +1,6 @@
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("copyr").innerHTML = `@ ${year} .:|:. Troy J. Nightingale .:|:. Minnesota`;
+
+const lastmod = document.querySelector('#lastmod');
+lastmod.textContent = `Last Updated: ${document.lastModified}`;
