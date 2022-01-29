2fa96daa4d48c474be83e9425c75b4413f673ec7
tnigh10 <tnigh10@byui.edu>
2022-01-29 02:09:50 -0600

    wrydfvbmilg

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -1,23 +1,13 @@
-const hambutton = document.querySelector('.ham');
-const mainnav = document.querySelector('.navigation')
-
-hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
-
-// To solve the mid resizing issue with responsive class on
-window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
-
-
-const d = new Date();
-let year = d.getFullYear();
-document.getElementById('#copyr').innerHTML = `@ ${year} .:|:. Troy J. Nightingale .:|:. Minnesota`;
-
-const lastmod = document.querySelector('#lastmod');
-lastmod.textContent = `Last Updated: ${document.lastModified}`;
-
-/* Header Date */
-const datefield = document.querySelector("date");
-
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
+  now
 );
+
+/*Date in header*/
+const datefieldUK = document.querySelector("date");
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+  dateStyle: "full",
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
