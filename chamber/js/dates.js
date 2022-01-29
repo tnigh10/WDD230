c0c67ca573cf8b15feade33c1c2cad2e240b72ea
tnigh10 <tnigh10@byui.edu>
2022-01-28 21:55:34 -0600

    update4

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -4,3 +4,11 @@ lastmod.textContent = `Last Updated: ${document.lastModified}`;
 const d = new Date();
 let year = d.getFullYear();
 document.getElementById("copyr").innerHTML = `@ ${year} .:|:. Troy J. Nightingale .:|:. Minnesota`;
+
+const hambutton = document.querySelector('.ham');
+const mainnav = document.querySelector('.navigation')
+
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// To solve the mid resizing issue with responsive class on
+window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
