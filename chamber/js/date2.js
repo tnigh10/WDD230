fc60f9ee5eff14384a80766cb4cfdb263b88e25b
tnigh10 <tnigh10@byui.edu>
2022-01-29 17:00:56 -0600

    iuhoiub

--- chamber/js/date2.js
+++ chamber/js/date2.js
@@ -1,7 +1,7 @@
 const now = new Date();
-const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-  now
-);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+  dateStyle: "full",
+}).format(now);
 
 /*Date in header*/
 const datefieldUK = document.querySelector("date");
@@ -11,3 +11,8 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 }).format(now);
 
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+  now
+);
--- chamber/js/fdates.js
+++ chamber/js/fdates.js
@@ -1,11 +1,6 @@
-/* Footer Date  */
-const d = new Date();
-let year = d.getFullYear();
-document.getElementById("#copyr").innerHTML = `@ ${year} .:|:. Troy J. Nightingale .:|:. Minnesota`;
-
-/* Footer Last Modification  */
 const lastmod = document.querySelector('#lastmod');
 lastmod.textContent = `Last Updated: ${document.lastModified}`;
 
-
-/*lastmod.textContent = `Last Updated: ${document.lastModified}`;*/
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("copyr").innerHTML = `@ ${year} .:|:. Troy J. Nightingale .:|:. Minnesota`;
