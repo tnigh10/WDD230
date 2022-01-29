8f5a7389a834412526d70d4b03c41391001a527f
tnigh10 <tnigh10@byui.edu>
2022-01-28 23:00:56 -0600

    mjhghm

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -1,12 +1,3 @@
-
-const d = new Date();
-let year = d.getFullYear();
-document.getElementById("copyr").innerHTML = `@ ${year} .:|:. Troy J. Nightingale .:|:. Minnesota`;
-
-const lastmod = document.querySelector('#lastmod');
-lastmod.textContent = `Last Updated: ${document.lastModified}`;
-
-
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
 
@@ -14,3 +5,19 @@ hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive'
 
 // To solve the mid resizing issue with responsive class on
 window.onresize = () => {if (window.innerWidth > 760) mainnav.classList.remove('responsive')};
+
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById('#copyr').innerHTML = `@ ${year} .:|:. Troy J. Nightingale .:|:. Minnesota`;
+
+const lastmod = document.querySelector('#lastmod');
+lastmod.textContent = `Last Updated: ${document.lastModified}`;
+
+/* Header Date */
+const datefield = document.querySelector("date");
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
