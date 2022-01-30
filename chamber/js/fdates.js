17d890cf84939e7a330ae05a15bf456226eccf97
tnigh10 <tnigh10@byui.edu>
2022-01-29 20:59:56 -0600

    bvspibn

--- chamber/js/fdates.js
+++ chamber/js/fdates.js
@@ -3,4 +3,4 @@ lastmod.textContent = `Last Updated: ${document.lastModified}`;
 
 const d = new Date();
 let year = d.getFullYear();
-document.getElementById("copyr").innerHTML = `@ ${year} .:|:. Troy J. Nightingale .:|:. Minnesota`;
+document.getElementById("copyr").innerHTML = `@ ${year} Lake Elmo Chamber .:|:. Troy Nightingale .:|:. WDD230 Project`;
