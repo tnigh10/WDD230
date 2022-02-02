45189e0ad2e96688a052bf26495ae32f62ffdd91
tnigh10 <tnigh10@byui.edu>
2022-02-01 21:33:19 -0600

    corrections

--- chamber/js/date2.js
+++ chamber/js/date2.js
@@ -1,16 +1,14 @@
 // select the elements to manipulate (output to)
-const datefield = document.querySelector("date");
-const datefieldUK = document.querySelector("date"); // for european/family history format with day first.
-
-// derive the current date using a date object
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
-	now
+  now
 );
-const fulldateUK = new Intl.DateTimeFormat("en-UK", {
-	dateStyle: "full"
-}).format(now);
-// long, medium, short options ... try them
 
-datefield.innerHTML = `<em>${fulldate}</em>`;
+/*Date in header*/
+const datefieldUK = document.querySelector("date");
+
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+  dateStyle: "full",
+}).format(now);
+
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
