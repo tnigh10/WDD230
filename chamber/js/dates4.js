81450d33fea800a416772facad09bb9f732932c7
tnigh10 <tnigh10@byui.edu>
2022-02-05 22:30:03 -0600

    last

--- chamber/js/dates4.js
+++ chamber/js/dates4.js
@@ -4,3 +4,14 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-UK", { dateStyle: "full" }).format(now);
 
 datefield.innerHTML = `<strong>${fulldate}</strong>`;
+
+/*banner JS */
+const day = current.getDay();
+const banner = document.querySelector('#banner')
+console.log(day)
+if (day == 1 || day == 2) {
+  banner.classList.add('show')
+}
+else {
+  banner.classList.add('hide')
+}
