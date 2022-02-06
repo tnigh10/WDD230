f5994b259fe39f7ed9856224d65aeb89b373605f
tnigh10 <tnigh10@byui.edu>
2022-02-05 20:51:32 -0600

    more updates

--- chamber/js/dates.js
+++ chamber/js/dates.js
@@ -11,3 +11,14 @@ const fulldateUK = new Intl.DateTimeFormat("en-UK", {
 }).format(now);
 
 datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
+
+/*banner JS */
+const day = current.getDay();
+const banner = document.querySelector('#banner')
+console.log(day)
+if (day == 5 || day == 6) {
+  banner.classList.add('show')
+}
+else {
+  banner.classList.add('hide')
+}
