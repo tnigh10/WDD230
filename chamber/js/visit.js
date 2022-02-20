4f3a1666e30818c89e54368365a7f114f3f57e8c
tnigh10 <tnigh10@byui.edu>
2022-02-19 23:20:30 -0600

    oinibuihbi

--- chamber/js/visit.js
+++ chamber/js/visit.js
@@ -6,10 +6,10 @@ let numVisits = Number(window.localStorage.getItem("visits-stored"));
 
 // determine if this is the first visit or display the number of visits.
 if (numVisits !== 0) {
-  visitsDisplay.textContent = numVisits;
-} else {
-  visitsDisplay.textContent = `This is your first visit!`;
-}
+    visitsDisplay.textContent = numVisits;
+  } else {
+    visitsDisplay.textContent = `This is your first visit!`;
+  }
 
 // increment the number of visits.
 numVisits++;
@@ -17,12 +17,36 @@ numVisits++;
 localStorage.setItem("visits-stored", numVisits);
 
 // show todays date.
-todayDisplay.textContent = Date.now();
+// todayDisplay.textContent = Date.now();
 
 
 // Initialize day counter
-const todayDisplay = document.querySelector(".today");
+todayDisplay.textContent = Date();
+//const todayDisplay = document.querySelector(".today");
 
 // get the stored value in localStorage
-let dayVisits = Number(window.localStorage.getItem("days-stored"));
+var numDays = String(window.localStorage.getItem("date-stored"));
+
+// To calculate the time difference of two dates
+var Difference_In_Time = todayDisplay.getTime() - numDays.getTime();
+  
+// To calculate the no. of days between two dates
+var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
+
+// determine how many days ago you last visited.
+if ( Difference_In_Days !== 0) {
+    
+    todayDisplay.textContent = Difference_In_Days;
+  } else {
+    todayDisplay.textContent = `You were here earlier today!`;
+  }
+
+  // Set new date for numDays
+  numDays = Date();
+
+  // store the new date of last visit value
+localStorage.setItem("date-stored", numDays);
+
+
+
 
