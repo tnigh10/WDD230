defe3ef36ce70a4398d59224f0131ae761287a6f
tnigh10 <tnigh10@byui.edu>
2022-03-09 20:39:21 -0600

    windy

--- lesson10/js/weather-api.js
+++ lesson10/js/weather-api.js
@@ -19,29 +19,31 @@ fetch(apiURL)
     document.querySelector("figcaption").textContent = desc;
 
 
+    let t = parseFloat(document.querySelector("#current-temp").textContent);
+    let s = parseFloat(document.querySelector("#current-wind").textContent);
+    let windchill = "";
+    
+    if (t <= 50 && s >= 3) {
+        windchill = windChill(t, s);
+        windchill = `${windchill}&#176;F`;
+    }   else {
+        windchill = "N/A";
+    }
+    
+    //  The output
+    document.querySelector("#w").innerHTML = windchill;
+    
+    
+    function windChill(t, s) {
+        windchill = (35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16));
+        return windchill.toFixed(0);
+    }
+
   });
 
   
 
-let t = parseFloat(document.querySelector("#current-temp").textContent);
-let s = parseFloat(document.querySelector("#current-wind").textContent);
-let windchill = "";
 
-if (t <= 50 && s >= 3) {
-    windchill = windChill(t, s);
-    windchill = `${windchill}&#176;F`;
-}   else {
-    windchill = "N/A";
-}
-
-//  The output
-document.querySelector("#w").innerHTML = windchill;
-
-
-function windChill(t, s) {
-    windchill = (35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16));
-    return windchill.toFixed(0);
-}
   //let temp = document.createElement("p");
 
   //temp.setAttribute("class", "temperature");
